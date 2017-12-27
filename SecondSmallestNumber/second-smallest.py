8c8e4a0a5daf394cf351f53747db23c06b286559
Chandan Singh <csrrocz@gmail.com>
2017-12-27 22:42:02 +0530

    Add files via upload

--- SecondSmallestNumber/second-smallest.py
+++ SecondSmallestNumber/second-smallest.py
@@ -1,1 +1,28 @@
-
+lst=[]
+minlst=[]
+maxlst=[]
+rstlst=[]
+for _ in range(int(raw_input())):
+    name = raw_input()
+    score = float(raw_input())
+    lst.append([score,name])
+mini=min(lst)
+maxi=max(lst)
+for i in range(0,len(lst)):
+    if lst[i][0]==mini[0]:
+        minlst.append(lst[i])
+    elif lst[i][0]==maxi[0]:
+        maxlst.append(lst[i])
+    else:
+        rstlst.append(lst[i])
+sortedlst=sorted(rstlst)
+if len(sortedlst)==0:
+    for i in range(0,len(maxlst)):
+        print maxlst[i][1]
+else:
+    minrstlst=min(sortedlst)
+    for i in range(0,len(sortedlst)):
+        if sortedlst[i][0]==minrstlst[0]:
+            print sortedlst[i][1]
+        else:
+            exit(0)
